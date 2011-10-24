473eea3878e4e61002caf86c283f7ec6b2ff6af8
David Powell <pwnzerdragoon@gmail.com>
2011-10-23 23:36:02 -0700

    All working now. Flutter added.

--- main.cpp
+++ main.cpp
@@ -8,42 +8,37 @@
 #define NUM_FLAKES 300
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 480
-#define PI 3.14159265358979323846
+#define PI 3.14159265358979
 
 
 /* Structs
  **********/
 typedef struct _Flake {
-    int x;
-    double y;
-    double gravity;
-    int wind;
-    double flutter;
+    double xOffset;         // X/Y offsets used to determine final screen location -
+    double yOffset;         //  otherwise they'd all be on top of each other
+    double gravity;         // Amount the snowflake falls in one millisecond
+    double wind;            // Like gravity, but for horizontal movement. May be negative.
+    double flutterRate;     // Rate at which the flake oscillates
+    double flutterOffset;   // Each flake's flutter starts at a different point else it looks like a damn snow disco
 } Flake;
 
 
-/* Globals
+/* Headers
  **********/
-SDL_Window* g_window;
 
 
 /* Functions
  ************/
-SDL_Renderer* init(void)
+int init(SDL_Window** wind, SDL_Renderer** rend)
 {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    
     //TODO: check for errors
     SDL_Init(SDL_INIT_VIDEO);
     
-    window = SDL_CreateWindow("Particle demo (Snow)",
-                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              SCREEN_WIDTH, SCREEN_HEIGHT,
-                              SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    g_window = window;
-    return renderer;
+    *wind = SDL_CreateWindow("Particle demo (Snow)",
+                             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                             SCREEN_WIDTH, SCREEN_HEIGHT,
+                             SDL_WINDOW_SHOWN);
+    *rend = SDL_CreateRenderer(*wind, -1, 0);
 }
 
 void initFlakes(Flake* flakes)
@@ -51,39 +46,26 @@ void initFlakes(Flake* flakes)
     Flake *flake;
     
     double timeToFall = 5.0;
+    double windRatio = 0.2;
+    double timeToFlutter = 2.0;
     
     for (int i = 0; i < NUM_FLAKES; i++)
     {
         flake = &flakes[i];
-        flake->x = rand() % 640;
-        flake->y = (double) rand() / RAND_MAX;
+        flake->xOffset = (double) rand() / RAND_MAX;
+        flake->yOffset = (double) rand() / RAND_MAX;
         flake->gravity = 1.0 / (timeToFall * 1000);
-        flake->wind = 1 + rand() % 3;
-        flake->flutter = ((double) rand() / RAND_MAX) * 2 * PI;
+        flake->wind = flake->gravity * windRatio;
+        flake->flutterRate = (2 * PI) / (timeToFlutter * 1000);
+        flake->flutterOffset = (2*PI) * ((double) rand() / RAND_MAX);
     }
 }
 
-void updateFlakes(Flake* flakes, int elapsed)
-{
-    Flake* flake;
-    for (int i = 0; i < NUM_FLAKES; i++)
-    {
-        flake = &flakes[i];
-//        flake->x += flake->wind;
-  //      if (flake->x > SCREEN_WIDTH)
-    //        flake->x = 0;
-        flake->y += flake->gravity * elapsed;
-        if (flake->y >= 1.0)
-            flake->y = 0.0;
-      //  flake->flutter += 0.1;
-    }
-}
-
-void render(SDL_Renderer* renderer, Flake* flakes)
-{
+void render(SDL_Renderer* renderer, Flake* flakes, unsigned int elapsed) {
     Flake* flake;
     int x;
     int y;
+    double flutter;
     
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
@@ -91,32 +73,33 @@ void render(SDL_Renderer* renderer, Flake* flakes)
     for (int i = 0; i < NUM_FLAKES; i++)
     {
         flake = &flakes[i];
-        x = flake->x + cos(flake->flutter) * 10;
-        y = SCREEN_HEIGHT * flake->y;
+        
+        flutter = cos(flake->flutterOffset + (flake->flutterRate * elapsed)) * 0.05;
+        x = fmod(flake->xOffset + (flake->wind * elapsed) + flutter, 1.0) * SCREEN_WIDTH;
+        y = fmod(flake->yOffset + (flake->gravity * elapsed), 1.0) * SCREEN_HEIGHT;
         SDL_RenderDrawPoint(renderer, x, y);
     }
     SDL_RenderPresent(renderer);
 }
 
-int main ( int argc, char** argv )
+int main (int argc, char** argv)
 {
+    SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Event event;
     int doQuit = false;
-    unsigned int lastFrameTick;
     unsigned int currentTick;
     int lastFps = 0;
     int framesThisSecond = 0;
     char windowTitle[100];      // WARNING - buffer overrun potential, check this at some point
     
     // Initialize code and get the renderer
-    renderer = init();
+    init(&window, &renderer);
     
     Flake flakes[NUM_FLAKES];
     initFlakes(flakes);
     
     // Main loop
-    lastFrameTick = SDL_GetTicks();
     while (doQuit == false)
     {
         while(SDL_PollEvent(&event))
@@ -130,18 +113,15 @@ int main ( int argc, char** argv )
             }
         }
         
-        // Currently capped at 50fps. Move to a framerate-independate design later
         currentTick = SDL_GetTicks();
-        updateFlakes(flakes, currentTick - lastFrameTick);
-        render(renderer, flakes);
+        render(renderer, flakes, currentTick);
         framesThisSecond++;
-        lastFrameTick = currentTick;
         
         if (currentTick - lastFps > 1000)
         {
             lastFps = currentTick;
             sprintf(windowTitle, "Snow particles demo - %dFPS", framesThisSecond);
-            SDL_SetWindowTitle(g_window, windowTitle);
+            SDL_SetWindowTitle(window, windowTitle);
             framesThisSecond = 0;
         }
     }
